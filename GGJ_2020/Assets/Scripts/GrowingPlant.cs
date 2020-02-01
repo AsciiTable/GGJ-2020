7b7b4c53a7fc7f7486dded07d0e1639ae5ee36ec
Jessica Wei <jessicawei9@gmail.com>
2020-02-01 06:08:54 -0800

    Ineed sleep

---
+++ GGJ_2020/Assets/Scripts/GrowingPlant.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrowingPlant : Plant
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        occupiedBlock = gameObject.GetComponentInParent<Block>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ GGJ_2020/Assets/Scripts/Plant.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public abstract class Plant : MonoBehaviour
+{
+    [SerializeField] protected string plantName;
+    [SerializeField] protected int growthTime;
+    [SerializeField] protected int spreadTime;
+    [SerializeField] protected bool destroyable;
+    protected Block occupiedBlock;
+}
