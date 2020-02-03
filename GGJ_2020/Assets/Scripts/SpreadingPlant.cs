38b0406bc00ca738e68fe063cc285d1ce518d13d
Jessica Wei <jessicawei9@gmail.com>
2020-02-03 00:33:51 -0800

    figuring out raycasts

--- GGJ_2020/Assets/Scripts/SpreadingPlant.cs
+++ GGJ_2020/Assets/Scripts/SpreadingPlant.cs
@@ -12,10 +12,10 @@ public class SpreadingPlant : MonoBehaviour
     public void Spread() {
         occupiedBlock = this.gameObject.GetComponent<GrowingPlant>().getOBlock();
         occupiedBlock.GetComponent<BoxCollider2D>().enabled = false;
-        Block bUp = checkCollision(Vector2.up);
-        Block bRight = checkCollision(Vector2.right);
-        Block bDown = checkCollision(Vector2.down);
-        Block bLeft = checkCollision(Vector2.left);
+        Block bUp = checkCollision(Vector2.up, (gm.BlockHeight+gm.BorderHeight)/2);
+        Block bRight = checkCollision(Vector2.right, (gm.BlockWidth + gm.BorderWidth)/2);
+        Block bDown = checkCollision(Vector2.down, (gm.BlockHeight + gm.BorderHeight) / 2);
+        Block bLeft = checkCollision(Vector2.left, (gm.BlockWidth + gm.BorderWidth) / 2);
 
         if (bUp != null) {
             bUp.Place(oPool.ID, false);
@@ -36,7 +36,7 @@ public class SpreadingPlant : MonoBehaviour
         occupiedBlock.GetComponent<BoxCollider2D>().enabled = true;
     }
 
-    protected Block checkCollision(Vector2 direction) {
+    protected Block checkCollision(Vector2 direction, float distance) {
         RaycastHit2D hit = Physics2D.Raycast(occupiedBlock.transform.position, direction, 1.5f);
         if (hit.collider != null && hit.collider.CompareTag("Block")) {
             Block b = hit.transform.gameObject.GetComponent<Block>();
