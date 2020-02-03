4b435343dacec23d782a92734743face031e66ac
Jessica Wei <jessicawei9@gmail.com>
2020-02-02 16:39:36 -0800

    fixing

--- GGJ_2020/Assets/Scripts/Tree.cs
+++ GGJ_2020/Assets/Scripts/Tree.cs
@@ -15,7 +15,7 @@ public class Tree : GrowingPlant
 
     public override void giveLifeToPlant()
     {
-        growTime = 2;
+        growTime = 1;
         occupiedBlock = associatedSeed.GetOccupiedBlock();
         growthStartDate = Plant.dayCount;
         plantID = Structs.id.tree;
