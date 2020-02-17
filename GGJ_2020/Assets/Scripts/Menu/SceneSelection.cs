a3fd8427f20379942895e62937875377f0b629ea
Jessica Wei <jessicawei9@gmail.com>
2020-02-17 01:01:08 -0800

    overworld working -- going to update some buttons next

---
+++ GGJ_2020/Assets/Scripts/Menu/SceneSelection.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSelection : MonoBehaviour
+{
+    //public string scene;
+    public void SceneSelector(string scene) {
+        SceneManager.LoadScene(scene);
+    }
+}
