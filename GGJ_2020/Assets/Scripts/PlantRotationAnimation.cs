16e880f535717ba0ae9e84562f792928a321c455
Jessica Wei <jessicawei9@gmail.com>
2020-03-28 06:46:57 -0700

    alternatives for flowers 1 and 2 uploaded

---
+++ GGJ_2020/Assets/Scripts/PlantRotationAnimation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantRotationAnimation : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer flowerSprites;
+
+    private void OnEnable()
+    {
+        //UpdateHandler.onUpdate += 
+    }
+    private void OnDisable()
+    {
+        //UpdateHandler.onUpdate -= 
+    }
+
+}
