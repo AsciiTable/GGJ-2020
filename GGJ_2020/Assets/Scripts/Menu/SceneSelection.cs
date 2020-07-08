289a2b279d648e13460a542534734023f237f084
Jessica Wei <jessicawei9@gmail.com>
2020-07-08 01:11:38 -0700

    tip slideshow in progress

--- GGJ_2020/Assets/Scripts/Menu/SceneSelection.cs
+++ GGJ_2020/Assets/Scripts/Menu/SceneSelection.cs
@@ -8,5 +8,12 @@ public class SceneSelection : MonoBehaviour
     public void SceneSelector(string scene) {
         SceneManager.LoadScene(scene);
     }
-    
+
+    public void GameObjectToggleOn(GameObject o) {
+        o.SetActive(true);
+    }
+
+    public void GameObjectToggleOff(GameObject o) {
+        o.SetActive(false);
+    }
 }
---
+++ GGJ_2020/Assets/Scripts/Menu/TipSlideshow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TipSlideshow : MonoBehaviour
+{
+    public List<GameObject> tips;
+    public GameObject backbutton;
+    public GameObject nextbutton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void incrementTip() { 
+    }
+
+    void decrementTip() { 
+    }
+}
